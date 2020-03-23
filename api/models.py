1ca09a726a97139497b7391508d3040b69492c6e
bad coder <2061469002@qq.com>
2020-03-23 17:11:16 +0800

    创建models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField('分类名', max_length=50)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    modified_time = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField('标签', max_length=50)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    modified_time = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField('标题', max_length=100)
+    excerpt = models.TextField('摘要', max_length=200, blank=True, help_text='默认取文章内容的前100个字符')
+    content = models.TextField('内容')
+    views = models.PositiveIntegerField(default=0, editable=False)
+    created_time = models.DateTimeField('创建时间', auto_now_add=True)
+    modified_time = models.DateTimeField('修改时间', auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='分类')
+    tag = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+
+    class Meta:
+        ordering = ['-created_time']
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'article_id': self.pk})
+
+    def save(self, *args, **kwargs):
+        self.excerpt = self.excerpt or self.content[:100]
+        super().save(*args, **kwargs)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
